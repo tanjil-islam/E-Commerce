55e19d335a6d0c9d4ef96da9fc7faf8e8560601f
tanjil-islam <tanjil.islam040820@gmail.com>
2026-06-23 04:51:39 +0600

    remove position prop from SelectContent

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -1,6 +1,5 @@
 import { SearchIcon } from 'lucide-react'
 import { Input } from '@/components/ui/input'
-
 import {
   Select,
   SelectContent,
@@ -9,10 +8,12 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import { APP_NAME } from '@/lib/constants'
+
 const categories = ['men', 'women', 'kids', 'accessories']
+
 export default async function Search() {
   return (
-    <form action='/search' method='GET' className='flex  items-stretch h-10 '>
+    <form action='/search' method='GET' className='flex items-stretch h-10'>
       <Select name='category'>
         <SelectTrigger className='w-auto h-full bg-gray-800 text-white border-gray-600 border-r-0 rounded-r-none rounded-l-md focus:ring-0'>
           <SelectValue placeholder='All' />
@@ -34,7 +35,7 @@ export default async function Search() {
       />
       <button
         type='submit'
-        className='bg-primary text-black rounded-l-none rounded-r-md h-full px-3 py-2 hover:bg-yellow-400 transition '
+        className='bg-primary text-black rounded-l-none rounded-r-md h-full px-3 py-2 hover:bg-yellow-400 transition'
       >
         <SearchIcon className='w-6 h-6' />
       </button>
